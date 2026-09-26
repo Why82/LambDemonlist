939bd6da834331fc77b3f643313fe2e8736bfb4a
Why82 <igors@buziaczek.pl>
2026-09-26 15:40:48 +0200

    Refactor LevelAuthors template for better clarity

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -1,4 +1,4 @@
-```js id="k9zq4m"
+```js
 export default {
     props: {
         author: {
@@ -21,13 +21,46 @@ export default {
         },
     },
 
-    template: '<div class="level-authors">' +
-        '<div class="type-title-sm">Creator</div>' +
-        '<p class="type-body"><span>{{ author }}</span></p>' +
-        '<div class="type-title-sm">Verifier</div>' +
-        '<p class="type-body"><span>{{ verifier }}</span></p>' +
-        '<div class="type-title-sm">Publisher</div>' +
-        '<p class="type-body"><span>{{ author }}</span></p>' +
-        '</div>',
+    template: `
+        <div class="level-authors">
+            <template v-if="selfVerified">
+                <div class="type-title-sm">Creator & Verifier</div>
+                <p class="type-body">
+                    <span>{{ author }}</span>
+                </p>
+            </template>
+
+            <template v-else-if="!creators || creators.length === 0">
+                <div class="type-title-sm">Creator</div>
+                <p class="type-body">
+                    <span>{{ author }}</span>
+                </p>
+
+                <div class="type-title-sm">Verifier</div>
+                <p class="type-body">
+                    <span>{{ verifier }}</span>
+                </p>
+            </template>
+
+            <template v-else>
+                <div class="type-title-sm">Creators</div>
+                <p class="type-body">
+                    <span v-for="(creator, index) in creators" :key="index">
+                        {{ creator }}<span v-if="index < creators.length - 1">, </span>
+                    </span>
+                </p>
+
+                <div class="type-title-sm">Verifier</div>
+                <p class="type-body">
+                    <span>{{ verifier }}</span>
+                </p>
+            </template>
+
+            <div class="type-title-sm">Publisher</div>
+            <p class="type-body">
+                <span>{{ author }}</span>
+            </p>
+        </div>
+    `,
 };
 ```
