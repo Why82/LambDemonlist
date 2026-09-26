a963fdcac34df62eba103a5876c964cde9a0232f
Why82 <igors@buziaczek.pl>
2026-09-26 15:38:08 +0200

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -1,4 +1,4 @@
-```js
+```js id="k9zq4m"
 export default {
     props: {
         author: {
@@ -17,50 +17,17 @@ export default {
 
     computed: {
         selfVerified() {
-            return this.author === this.verifier && this.creators.length === 0;
+            return this.author === this.verifier && (!this.creators || this.creators.length === 0);
         },
     },
 
-    template: `
-        <div class="level-authors">
-            <template v-if="selfVerified">
-                <div class="type-title-sm">Creator & Verifier</div>
-                <p class="type-body">
-                    <span>{{ author }}</span>
-                </p>
-            </template>
-
-            <template v-else-if="creators.length === 0">
-                <div class="type-title-sm">Creator</div>
-                <p class="type-body">
-                    <span>{{ author }}</span>
-                </p>
-
-                <div class="type-title-sm">Verifier</div>
-                <p class="type-body">
-                    <span>{{ verifier }}</span>
-                </p>
-            </template>
-
-            <template v-else>
-                <div class="type-title-sm">Creators</div>
-                <p class="type-body">
-                    <span v-for="(creator, index) in creators" :key="index">
-                        {{ creator }}<span v-if="index < creators.length - 1">, </span>
-                    </span>
-                </p>
-
-                <div class="type-title-sm">Verifier</div>
-                <p class="type-body">
-                    <span>{{ verifier }}</span>
-                </p>
-            </template>
-
-            <div class="type-title-sm">Publisher</div>
-            <p class="type-body">
-                <span>{{ author }}</span>
-            </p>
-        </div>
-    `,
+    template: '<div class="level-authors">' +
+        '<div class="type-title-sm">Creator</div>' +
+        '<p class="type-body"><span>{{ author }}</span></p>' +
+        '<div class="type-title-sm">Verifier</div>' +
+        '<p class="type-body"><span>{{ verifier }}</span></p>' +
+        '<div class="type-title-sm">Publisher</div>' +
+        '<p class="type-body"><span>{{ author }}</span></p>' +
+        '</div>',
 };
 ```
