62f5fb2f29ee41120fcaf678ae2bfa6ec71516bf
Why82 <igors@buziaczek.pl>
2026-09-26 13:55:46 +0200

    Refactor template structure for creators display

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -22,6 +22,7 @@ export default {
                     <span>{{ author }}</span>
                 </p>
             </template>
+
             <template v-else-if="creators.length === 0">
                 <div class="type-title-sm">Creator</div>
                 <p class="type-body">
@@ -32,19 +33,25 @@ export default {
                     <span>{{ verifier }}</span>
                 </p>
             </template>
+
             <template v-else>
                 <div class="type-title-sm">Creators</div>
                 <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span>{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
+                    <template
+                        v-for="(creator, index) in creators"
+                        :key="\`creator-\$\{creator\}\`"
+                    >
+                        <span>{{ creator }}</span>
+                        <span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
+
                 <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
             </template>
+
             <div class="type-title-sm">Publisher</div>
             <p class="type-body">
                 <span>{{ author }}</span>
@@ -56,6 +63,4 @@ export default {
         selfVerified() {
             return this.author === this.verifier && this.creators.length === 0;
         },
-    },
-};
 ```
